c3b6631eac677243aaf74f1c4ff2ed18d787b03d
DeltaV235 <wuyue96@gmail.com>
2020-10-02 17:07:05 +0800

    feat: add 00-main.sh which the entry point of scirpt

---
+++ 8-linux-inital-script/00-main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### descrption: main script, the entry point of program
+### create time: 2020-10-02 (yyyy-MM-dd)
+### author: deltaV235
+
+source ./assets/00-assets-index.sh
+
+bash ./01-change-package-download-mirror-to-local.sh
+
